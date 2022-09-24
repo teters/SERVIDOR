6cbc8af49419993672e64a01e72f130d3c18807e
Juan Lasala <jlasala@correo.um.edu.uy>
2022-09-23 21:07:48 -0300

    Creo EmpresasRepository

--- PrimerProyectoTIC1/src/main/java/com/example/PrimerProyectoTIC1/EmpresasRepository.java
+++ PrimerProyectoTIC1/src/main/java/com/example/PrimerProyectoTIC1/EmpresasRepository.java
@@ -4,5 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmpresasRepository extends JpaRepository<Empresa, String>{
-}
+public interface EmpresasRepository extends JpaRepository<Empresa, String>{}
