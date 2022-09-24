0076382dbc8bf94520f25a0de183c74749066a88
Juan Lasala <jlasala@correo.um.edu.uy>
2022-09-23 21:02:31 -0300

    EmpresasRepository

---
+++ PrimerProyectoTIC1/src/main/java/com/example/PrimerProyectoTIC1/EmpresasRepository.java
@@ -0,0 +1,8 @@
+package com.example.PrimerProyectoTIC1;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmpresasRepository extends JpaRepository<Empresa, String>{
+}
