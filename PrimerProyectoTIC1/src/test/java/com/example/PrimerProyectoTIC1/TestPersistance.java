728b2ddd512d5937e63273129a05a32c48ecff93
Juan Lasala <jlasala@correo.um.edu.uy>
2022-09-30 20:11:44 -0300

    Test de persistencia de bossEmpresa

---
+++ PrimerProyectoTIC1/src/test/java/com/example/PrimerProyectoTIC1/TestPersistance.java
@@ -0,0 +1,14 @@
+package com.example.PrimerProyectoTIC1;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class TestPersistance {
+
+    BossEmpresa boss;
+
+    @BeforeEach
+    void setUp(){
+        boss = new BossEmpresa();
+    }
+
+}
