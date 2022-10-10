ba4d8992052788611af0266e6b96be1344081ef5
Juan Lasala <jlasala@correo.um.edu.uy>
2022-10-10 10:20:26 -0300

    solucion de errores

--- PrimerProyectoTIC1/src/main/java/com/example/PrimerProyectoTIC1/User/BossCD1.java
+++ PrimerProyectoTIC1/src/main/java/com/example/PrimerProyectoTIC1/User/BossCD1.java
@@ -7,10 +7,6 @@ import javax.persistence.Table;
 @Table */ //(name = "Boss Centro Deportivo")
 // esta tabla no deberia persistir en la BD
 public class BossCD1 extends User1 {
-    private String nombre;
-    private String direccion;
-
-
 
 
     public BossCD1() {
